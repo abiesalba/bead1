89951a6c45f9b7cb38fb57d04f080bcc72de6bea
abiesalba <abies_alba@inf.elte.hu>
2018-10-20 16:30:55 +0200

    created the Sudoku class

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -6,23 +6,34 @@
 double vector_length(const std::vector<double>& data)
 {
 	double square_length = 0;
-	
+
 	for (const double& num : data)
 	{
 		square_length += (num * num);
 	}
-	
+
 	return std::sqrt(square_length);
 }
+
+class Sudoku{
+private:
+    unsigned _size;
+    vector<vector<unsigned>> _sudoku;
+
+public:
+    unsigned get_size(){ return _size;}
+    vector<vector<unsigned>> getSudoku(){return _sudoku;}
+
+};
 
 int main()
 {
 	std::ifstream input("input.txt");
-	
+
 	unsigned int N, M;
-	
+
 	input >> N >> M;
-	
+
 	// Read all the data into one array
 	std::vector<std::vector<double>> data(N, std::vector<double>(M));
 	for (int i = 0; i < N; i++)
@@ -32,16 +43,16 @@ int main()
 			input >> data[i][j];
 		}
 	}
-	
+
 	input.close();
-	
+
 	// Start calculating length for every vector.
 	std::vector<std::future<double>> results;
 	for (size_t i = 0; i < N; i++)
 	{
 		results.push_back(std::async(std::launch::async, vector_length, data[i]));
 	}
-	
+
 	// Wait for the function to return, write the result into a file
 	std::ofstream output("output.txt");
 	for (std::future<double>& f : results)
@@ -50,6 +61,6 @@ int main()
 		output << f.get() << std::endl;
 	}
 	output.close();
-	
+
 	return 0;
 }
